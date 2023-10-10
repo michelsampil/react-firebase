1416e6cfe2c519cb834985c5727b82c22a04d812
△ Michel Sampil <michel.sampil@gmail.com>
2023-10-09 22:42:26 -0300

    basic redux firebase

---
+++ src/components/Cart/CartItem.jsx
@@ -0,0 +1,58 @@
+import { useDispatch } from "react-redux";
+
+import classes from "./CartItem.module.css";
+import { cartActions } from "../../store/cart-slice";
+import PropTypes from "prop-types";
+
+const CartItem = (props) => {
+  const dispatch = useDispatch();
+
+  const { title, quantity, total, price, id } = props.item;
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
+
+  const addItemHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
+  return (
+    <li className={classes.item}>
+      <header>
+        <h3>{title}</h3>
+        <div className={classes.price}>
+          ${total.toFixed(2)}{" "}
+          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+        </div>
+      </header>
+      <div className={classes.details}>
+        <div className={classes.quantity}>
+          x <span>{quantity}</span>
+        </div>
+        <div className={classes.actions}>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemHandler}>+</button>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+CartItem.propTypes = {
+  item: {
+    title: PropTypes.string,
+    quantity: PropTypes.string,
+    total: PropTypes.number,
+    price: PropTypes.number,
+    id: PropTypes.string,
+  },
+};
+
+export default CartItem;
---
+++ src/components/Shop/ProductItem.jsx
@@ -0,0 +1,49 @@
+import { useDispatch } from "react-redux";
+import PropTypes from "prop-types";
+
+import { cartActions } from "../../store/cart-slice";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+
+const ProductItem = (props) => {
+  const dispatch = useDispatch();
+
+  const { title, price, description, id } = props;
+
+  const addToCartHandler = () => {
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <header>
+          <h3>{title}</h3>
+          <div className={classes.price}>${price.toFixed(2)}</div>
+        </header>
+        <p>{description}</p>
+        <div className={classes.actions}>
+          <button onClick={addToCartHandler}>Add to Cart</button>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+ProductItem.propTypes = {
+  title: PropTypes.string,
+  price: PropTypes.number,
+  description: PropTypes.string,
+  id: PropTypes.string,
+};
+
+export default ProductItem;
